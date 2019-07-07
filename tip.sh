f5a49df98e095f8dcd1b7156b2d3b5bcfeb5d774
jessiceasy <45992005+jessiceasy@users.noreply.github.com>
2019-07-07 17:51:15 +0800

    Update tip.sh

--- tip.sh
+++ tip.sh
@@ -2,17 +2,24 @@
 
 lww_keys[0]='tar 0'
 lww_keys[1]='find 1'
+lww_keys[a]='path 2'
 
 lww_tips[0]='压缩前什么样子解压后就是什么样子，不会自建目录\n
 tar -czvf ***.tar.gz 要压缩的文件或目录 #gz\n
 tar -cjvf ***.tar.bz2 要压缩的文件或目录 #bz2\n
 tar -xzvf ***.tar.gz  #解压gz\n
 tar -xjvf ***.tar.bz2 #解压bz2\n'
+
 lww_tips[1]='find  -mindepth 1  -maxdepth 2 -not -name init.sh -o -name  "aaa*" #搜索范围是从第一层到第二次包括第一层和第二层，是一个全局的限制即逻辑符号-a,-o前后都管用\n
 find -maxdepth 1 -not -name init.sh # 逻辑not\n
 find -maxdepth 1 -not -name init.sh -a -not -name "bbbb*"   #逻辑与\n
 find -maxdepth 2  -not -name init.sh -o -not -name "bbbb*"  #逻辑或\n
 find find *.c -exec cp "'"{}"'" /tmp \; #拷贝查找结果至tmp  "\;"为固定格式  需要将大括号外面的双引号变为改为单引号\n'
+
+lww_tips[2]='环境变量设置，脚本转化的可执行文件建议放在~/.path下面\n
+zsh 在 ~/.zshrc 结尾增加 export PATH=~/.path:$PATH \n
+bash 在 ~/.bashrc 结尾增加 export PATH=~/.path:$PATH \n'
+
 
 
 
